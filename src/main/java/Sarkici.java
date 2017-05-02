c530cdf99274eeffe49be60711e0efd25a0d8f5c
ffmete <fatih.furkan.mete@hotmail.com>
2017-05-02 23:41:26 +0300

    proje2

---
+++ src/main/java/Sarkici.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Mete on 25.04.2017.
+ */
+public class Sarkici {
+
+    public int Id;
+    public String Ad,SoyAd;
+
+    public Sarkici(int id, String ad, String soyAd) {
+        this.Id = id;
+        this.Ad = ad;
+        this.SoyAd = soyAd;
+    }
+}
